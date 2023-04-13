e0793e3db8c56cc664826df601b59c82c234ac9e
HyeonKyuChoi <71831720+chk0527@users.noreply.github.com>
2023-04-13 16:34:56 +0900

    react test
    commit1

---
+++ react/aaa.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const aaa = () => {
+  return (
+    <View>
+      <Text>aaa</Text>
+    </View>
+  )
+}
+
+export default aaa
+
+const styles = StyleSheet.create({})
