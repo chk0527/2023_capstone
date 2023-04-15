98e91db4c18ece9ecd1dc2be65749ada3c44cc18
chk0527 <chlgusrb0213@naver.com>
2023-04-15 22:26:32 +0900

    서버 초안

---
+++ server/Springserver/src/main/java/com/example/springserver/Controller.java
@@ -0,0 +1,44 @@
+package com.example.springserver;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+@RequiredArgsConstructor
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Test Message22222";
+    }
+
+    @Autowired
+    final UserService userService;
+
+    final UserRepository userRepository;
+
+    @GetMapping("/data")
+    public List<Integer> getData() {
+        return userRepository.findTimeStampByNameAndObject();
+    }
+
+    @GetMapping("/dataall/")
+    public List<User> getData2() {
+        return userRepository.findByNameAndObject();
+    }
+
+    @GetMapping("/alldata")
+    public List<User> getData3() {
+        return userRepository.findAllData();
+    }
+
+
+}
---
+++ server/Springserver/src/main/java/com/example/springserver/UserService.java
@@ -0,0 +1,29 @@
+package com.example.springserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public List<Integer> getUsersByNameAndObject() {
+        return userRepository.findTimeStampByNameAndObject();
+    }
+
+    public List<User> getTimestampByNameAndObject() {
+        return userRepository.findByNameAndObject();
+    }
+
+    public List<User> getAllData() {
+        return userRepository.findAllData();
+    }
+}
