e35dfe7276e22c38d09fb66ee6e0356f5ec263d1
chk0527 <chlgusrb0213@naver.com>
2023-05-27 22:03:42 +0900

    서버 배포 테스트부분 주석 처리 테스트

--- server/Springserver/src/test/java/com/example/springserver/SpringserverApplicationTests.java
+++ server/Springserver/src/test/java/com/example/springserver/SpringserverApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.springserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringserverApplicationTests {
 
-    @Test
+    //@Test
     void contextLoads() {
     }
 
